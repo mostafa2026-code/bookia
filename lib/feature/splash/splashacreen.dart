f62556fbfd577efa99919632fa44d596a81465de
mostafa2026-code <mustafaabdelrahim2024@gmail.com>
2025-09-18 00:03:06 +0300

    add dependeces

---
+++ lib/feature/splash/splashacreen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+
+class Splashacreen extends StatefulWidget {
+  const Splashacreen({super.key});
+
+  @override
+  State<Splashacreen> createState() => _SplashacreenState();
+}
+
+class _SplashacreenState extends State<Splashacreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center
+        ,
+        children: [
+         
+        ],
+      ),
+    );
+  }
+}
