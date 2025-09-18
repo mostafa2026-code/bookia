f3c3576beae0bd417e542b37f34396729b5a2da2
mostafa2026-code <mustafaabdelrahim2024@gmail.com>
2025-09-18 12:00:07 +0300

    add password change screen

--- lib/feature/auth/passwordchanged/passwordchnagedScreen.dart
+++ lib/feature/auth/passwordchanged/passwordchnagedScreen.dart
@@ -1,10 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class Passwordchnagedscreen extends StatelessWidget {
   const Passwordchnagedscreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(24),
+          child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                GestureDetector(
+                  child: Container(
+                    height: 41,
+                    width: 41,
+                    padding: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      border: Border.all(color: Colors.black, width: 0.7),
+                    ),
+                    child: Center(child: Icon(Icons.arrow_back_ios)),
+                  ),
+                ),
+                Gap(32),
+                Text("Forgot Password?"),
+                Text(
+                  "Don't worry! It occurs. Please enter the email address linked with your account.",
+                ),
+                Gap(32),
+                TextFormField(decoration: InputDecoration(hintText: "Username")),
+                Gap(32),
+                Center(child: ElevatedButton(onPressed: () {}, child: Text("Send Code"))),
+                Gap(32),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Remember Password?"),
+                      TextButton(onPressed: () {}, child: Text("Login")),
+                    ],
+                  ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/feature/auth/register/registerscreen.dart
+++ lib/feature/auth/register/registerscreen.dart
@@ -6,59 +6,61 @@ class Registerscreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(24),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              GestureDetector(
-                child: Container(
-                  height: 41,
-                  width: 41,
-                  padding: const EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(12),
-                    border: Border.all(color: Colors.black, width: 0.7),
+    return SingleChildScrollView(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(24),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                GestureDetector(
+                  child: Container(
+                    height: 41,
+                    width: 41,
+                    padding: const EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      border: Border.all(color: Colors.black, width: 0.7),
+                    ),
+                    child: Center(child: Icon(Icons.arrow_back_ios)),
                   ),
-                  child: Center(child: Icon(Icons.arrow_back_ios)),
                 ),
-              ),
-              Gap(32),
-              Text("Welcome back! Glad\nto see you, Again!"),
-              Gap(32),
-              TextFormField(decoration: InputDecoration(hintText: "Username")),
-              Gap(16),
-              TextFormField(decoration: InputDecoration(hintText: "Email")),
-              Gap(16),
-          
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: "Password",
-                  suffixIcon: Icon(Icons.visibility_off),
+                Gap(32),
+                Text("Welcome back! Glad\nto see you, Again!"),
+                Gap(32),
+                TextFormField(decoration: InputDecoration(hintText: "Username")),
+                Gap(16),
+                TextFormField(decoration: InputDecoration(hintText: "Email")),
+                Gap(16),
+            
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Password",
+                    suffixIcon: Icon(Icons.visibility_off),
+                  ),
                 ),
-              ),
-              Gap(16),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: "Confirm Password",
-                  suffixIcon: Icon(Icons.visibility_off),
+                Gap(16),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Confirm Password",
+                    suffixIcon: Icon(Icons.visibility_off),
+                  ),
                 ),
-              ),
-              Gap(32),
-              Center(
-                child: ElevatedButton(onPressed: () {}, child: Text("Register")),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("Don’t have an account? "),
-                  TextButton(onPressed: () {}, child: Text("Register Now")),
-                ],
-              ),
-            ],
+                Gap(32),
+                Center(
+                  child: ElevatedButton(onPressed: () {}, child: Text("Register")),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Don’t have an account? "),
+                    TextButton(onPressed: () {}, child: Text("Register Now")),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
