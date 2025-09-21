12389147e13931bb73181f15d72f97ad471f70cc
mostafa2026-code <mustafaabdelrahim2024@gmail.com>
2025-09-21 23:07:38 +0300

    add packages

---
+++ lib/core/routes/myroutes.dart
@@ -0,0 +1,15 @@
+class MyRouts{
+  static const String splash = '/';
+  static const String welcome = '/welcome';
+  static const String home = '/home';
+  static const String login = '/login';
+  static const String register = '/signup';
+  static const String createpassword = '/createpassword';
+  static const String createnewpassword = '/createnewpassword';
+  static const String otpverication = '/otpverication';
+  static const String profile = '/profile';
+  static const String editprofile = '/editprofile';
+  static const String signup = '/signup';
+  
+
+}
