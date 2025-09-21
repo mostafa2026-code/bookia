9add05fd2d789ee0df04863d7542434930082773
mostafa2026-code <mustafaabdelrahim2024@gmail.com>
2025-09-21 23:28:20 +0300

    add go routes

--- lib/core/routes/myroutes.dart
+++ lib/core/routes/myroutes.dart
@@ -1,15 +1,76 @@
-class MyRouts{
+import 'package:bookia/feature/auth/login/pages/loginscreen.dart';
+import 'package:bookia/feature/auth/otpverication/pages/otpvericationscreen.dart';
+import 'package:bookia/feature/auth/register/pages/registerscreen.dart';
+import 'package:bookia/feature/home/pages/homescreen.dart';
+import 'package:bookia/feature/profile/profilemain/pages/profilescreen.dart';
+import 'package:bookia/feature/profile/updateprofile/editptofile/editprofilescreen.dart';
+import 'package:bookia/feature/splash/splashacreen.dart';
+import 'package:bookia/feature/welcome/welcomescreen.dart';
+import 'package:go_router/go_router.dart';
+
+class MyRouts {
   static const String splash = '/';
   static const String welcome = '/welcome';
   static const String home = '/home';
   static const String login = '/login';
   static const String register = '/signup';
-  static const String createpassword = '/createpassword';
-  static const String createnewpassword = '/createnewpassword';
+  
   static const String otpverication = '/otpverication';
   static const String profile = '/profile';
   static const String editprofile = '/editprofile';
-  static const String signup = '/signup';
-  
 
+  final GoRouter myroutes = GoRouter(
+    routes: [
+      GoRoute(
+        path: splash,
+        builder: (context, state) {
+          return const Splashacreen();
+        },
+      ),
+      GoRoute(
+        path: welcome,
+        builder: (context, state) {
+          return const Welcomescreen();
+        },
+      ),
+      GoRoute(
+        path: home,
+        builder: (context, state) {
+          return const Homescreen();
+        },
+      ),
+      GoRoute(
+        path: login,
+        builder: (context, state) {
+          return const Loginscreen();
+        },
+      ),
+      GoRoute(
+        path: register,
+        builder: (context, state) {
+          return const Registerscreen();
+        },
+      ),
+      GoRoute(
+        path:otpverication ,
+        builder: (context, state) {
+          return  Otpvericationscreen();
+        },
+      ),
+      GoRoute(
+        path: profile,
+        builder: (context, state) {
+          return const Profilescreen();
+        },
+      ),
+      GoRoute(
+        path: editprofile,
+        builder: (context, state) {
+          return const Editprofilescreen();
+        },
+      ),
+      
+      
+    ],
+  );
 }
